ef073fb8971f976ca2395456244601bc6f3b0b85
beluchiigbo <queenbeluchi@gmail.com>
2023-09-20 02:18:08 -0500

    JS for tab & reorganized html

--- Assets/js/bulma.js
+++ Assets/js/bulma.js
@@ -6,5 +6,27 @@ burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
 
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+// add event listener to each of the tabs; remove is-active class to click on diff tabs
+tabs.forEach((tab) => {
+    tab.addEventListener('click' , () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+// show hidden tabs - isnt working yet
+    const target = tab.dataset.target;
+    tabContentBoxes.forEach(box => {
+        if (box.getAttribute('id') === target) {
+            box.classList.remove('is-hidden');
+        } else {
+            box.classList.add('is-hidden');
+        }
+        });
+    })
+  }) 
+
 
 

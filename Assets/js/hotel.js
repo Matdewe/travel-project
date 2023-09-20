62992ec4393ed66827558ea38b9f66817411d8f8
Matthew <Deweasematthew@gmail.com>
2023-09-20 17:42:01 -0500

    Js update hotel

--- Assets/js/hotel.js
+++ Assets/js/hotel.js
@@ -1,23 +1,35 @@
-fetchData();
-async function fetchData() {
-const url = "https://hotels4.p.rapidapi.com/locations/v3/search?q=United%20States&locale=en_US&langid=1033&siteid=300000001";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "76a001a6c0mshaa5aee7010710afp13ac34jsn3f6097c6aac3",
-    "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-  },
+const fetchHotels = () => {
+  // gets user input const userInput = document.querySelector("#userQuery").value;
+
+  const url =
+    "https://hotels4.p.rapidapi.com/locations/v3/search?q=United%20States&locale=en_US&langid=1033&siteid=300000001";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "76a001a6c0mshaa5aee7010710afp13ac34jsn3f6097c6aac3",
+      "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Need to be fixed");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const resultsDiv = document.querySelector("#results");
+      // resultsDiv.innerHTML = JSON.stringify(data);
+
+      for (let i = 0; i < data.sr.length; i++) {
+        let myElm = document.createElement("p");
+        myElm.textContent = data.sr[i].regionNames.primaryDisplayName;
+        resultsDiv.appendChild(myElm);
+      }
+    })
+
+    .catch((error) => console.error("Error", error));
 };
-
-try {
-  const response = await fetch(url , options);
-  const result = await response.text();
-
-const data = JSON.parse(result);
-
-console.log(data);
-} catch(error) {
-console.error(error);
-
-}
-} fetchData();
+fetchHotels();

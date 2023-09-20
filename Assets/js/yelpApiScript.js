8998f3335012de501c11868fd8abed26d4279f83
Megan Wright <meganmwright2018@gmail.com>
2023-09-19 21:17:33 -0500

    working through brain mess, trying to find a working API

--- Assets/js/yelpApiScript.js
+++ Assets/js/yelpApiScript.js
@@ -1,6 +1,6 @@
-fetch('https://api.yelp.com/v3/events/awesome-event', {
-    headers: {
-        'Authorization': 'Bearer API_KEY',
-        'accept': 'application/json'
-    }
-});
+// fetch('https://api.yelp.com/v3/events', {
+//     headers: {
+//         'Authorization': 'Bearer API_KEY',
+//         'accept': 'application/json'
+//     }
+// });
